90ffd9e49e7603f08891f7b815f720639a61909b
Pavlo <pkhasanov@gmail.com>
2023-10-09 20:48:09 +0300

    Add project files.

---
+++ CW_Delegate_C_Sharp/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Delegate_C_Sharp
+{
+    internal class Message
+    {
+        string _messgae;
+
+        public Message(string message)
+        {
+            _messgae = message;
+        }
+
+        public override string ToString()
+        {
+            return _messgae;
+        }
+
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ CW_Delegate_C_Sharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Delegate_C_Sharp
+{
+    delegate void ShowMessage(string mess);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message1 = new Message("Hello");
+            Message messgae2 = new Message(", world!");
+
+            ShowMessage getMessage = new ShowMessage(Message.DisplayMessage);
+            getMessage.Invoke(message1.ToString()); 
+            getMessage.Invoke(messgae2.ToString());
+            Console.ReadLine();
+        }
+    }
+}
